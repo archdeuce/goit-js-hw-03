0fc5f7928db76d2524e159f15889c9581917e938
Oleg Reshetilo <archdeuce@gmail.com>
2020-11-20 23:14:35 +0200

    goit-js-hw-03

---
+++ scripts/task-3.js
@@ -0,0 +1,44 @@
+const findBestEmployee = function (employees) {
+  let name = "";
+  let tasks = 0;
+
+  const keys = Object.keys(employees);
+
+  for (let key of keys) {
+    if (employees[key] > tasks) {
+      tasks = employees[key];
+      name = key;
+    }
+  }
+
+  return name;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  })
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  })
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  })
+); // lux
---
+++ scripts/task-4.js
@@ -0,0 +1,31 @@
+const countTotalSalary = function (employees) {
+  let sum = 0;
+  const keys = Object.keys(employees);
+
+  for (let key of keys) {
+    sum += employees[key];
+  }
+
+  return sum;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
